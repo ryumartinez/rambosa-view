001cb3bed72407a0fad67e0e0a31aa4a386877d2
ryumartinez <ryueduardomartinez@outlook.es>
2024-03-23 13:42:57 -0300

    moved navbar and footer to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
   import '../reset.css'
   import '../main-grid.css'
+	import Navbar from '../components/Navbar.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
   
-
 <div class="main-grid">
+  <Navbar/>
   <slot/>
+  <Footer/>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,7 @@
 <script>
-
-	import Footer from '../components/Footer.svelte';
   import Menu from '../components/Menu.svelte';
-  import Navbar from '../components/Navbar.svelte';
-
 </script>
 
-<Navbar/>
 <Menu></Menu>
-<Footer></Footer>
+
 
